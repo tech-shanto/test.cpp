54271b8ca0fcee8d3e4b6defb0b8a83f01b81747
Jahid Hasan <jahid.hasan.shanto@|gmail.com>
2024-11-17 23:37:52 +0600

    dynamic variable

--- test.cpp
+++ test.cpp
@@ -1,13 +1,10 @@
 #include<bits/stdc++.h>// all header file included.
 using namespace std;
 int main(){
-    double d = 20.75756;
-    cout<<fixed<<setprecision(2)<<d<<endl;//must be need fixed then setprecision function and 1st bracket then declare  how much digit we need to print
+    int x =10;
+    int *p = new int;
+    *p = 100;
+    cout << *p << endl;
     
-    
-    
-    // int a; //initialize data type and declare variable
-    // cin >> a; //scan form input.txt file
-    // cout << a <<endl;//print it into output.txt file
     return 0;
 }
