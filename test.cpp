ad35280ed0a7ce041d0ad62a92fb2c51064853a4
Jahid Hasan <jahid.hasan.shanto@|gmail.com>
2024-11-17 18:49:44 +0600

    setprecision function iomanip

--- test.cpp
+++ test.cpp
@@ -1,14 +1,12 @@
 #include<iostream>
+#include<iomanip>//input output manipulation
 using namespace std;
 int main(){
-    int eof;
-    while (cin >> eof)//while loop
-    {
-        cout << eof << endl;//end of file input system 
-    }
+    double d = 20.75756;
+    cout<<fixed<<setprecision(2)<<d<<endl;//must be need fixed then setprecision function and 1st bracket then declare  how much digit we need to print
     
-
-
+    
+    
     // int a; //initialize data type and declare variable
     // cin >> a; //scan form input.txt file
     // cout << a <<endl;//print it into output.txt file
