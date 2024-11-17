948376e61ee07060c9a764cd08c6f5f9c6a574cb
Jahid Hasan <jahid.hasan.shanto@|gmail.com>
2024-11-17 18:36:36 +0600

    while loop end of file input

--- test.cpp
+++ test.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a; //initialize data type and declare variable
-    cin >> a; //scan form input.txt file
-    cout << a <<endl;//print it into output.txt file
+    int eof;
+    while (cin >> eof)
+    {
+        cout << eof << endl;
+    }
+    
+
+
+    // int a; //initialize data type and declare variable
+    // cin >> a; //scan form input.txt file
+    // cout << a <<endl;//print it into output.txt file
     return 0;
 }
