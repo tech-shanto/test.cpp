5767b41c796350d4bd9d071b77a569e332fedd64
Jahid Hasan <jahid.hasan.shanto@|gmail.com>
2024-11-21 23:50:22 +0600

    sorting ascending & descending

--- test.cpp
+++ test.cpp
@@ -1,10 +1,26 @@
-#include<bits/stdc++.h>// all header file included.
+#include <bits/stdc++.h> // all header files included.
 using namespace std;
-int main(){
-    int x =10;
-    int *p = new int;
-    *p = 100;
-    cout << *p << endl;
-    
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+    sort(A.begin(), A.end());
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    sort(A.begin(), A.end(), greater<int>());
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
