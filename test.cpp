4ab9d364f6d0f542d4f12dc0ee639874c17601be
Jahid Hasan <jahid.hasan.shanto@|gmail.com>
2024-12-02 18:26:04 +0600

    sort it bt multiple entry

--- test.cpp
+++ test.cpp
@@ -3,32 +3,38 @@ using namespace std;
 
 int main()
 {
-    int N;
+   int N;
     cin >> N;
     vector<tuple<string, int, char, int, int, int>> students(N);
-    for (int i = 0; i < N; ++i)
-    {
+    
+
+    for (int i = 0; i < N; ++i) {
         string nm;
         int cls, id, math_marks, eng_marks;
         char s;
         cin >> nm >> cls >> s >> id >> math_marks >> eng_marks;
         students[i] = make_tuple(nm, cls, s, id, math_marks, eng_marks);
     }
-    sort(students.begin(), students.end(), [](const auto &a, const auto &b)
-         {
-        int total_marks_a = get<4>(a) + get<5>(a);
-        int total_marks_b = get<4>(b) + get<5>(b);
-        if (total_marks_a == total_marks_b) {
-            return get<3>(a) < get<3>(b);
+    sort(students.begin(), students.end(), [](const auto& a, const auto& b) {
+        int eng_a = get<5>(a);
+        int eng_b = get<5>(b);
+        if (eng_a == eng_b) {
+            int math_a = get<4>(a);
+            int math_b = get<4>(b);
+            if (math_a == math_b) {
+                return get<3>(a) < get<3>(b);
+            }
+            return math_a > math_b; 
         }
-        return total_marks_a > total_marks_b; });
-    for (const auto &student : students)
-    {
-        cout << get<0>(student) << " "
-             << get<1>(student) << " "
-             << get<2>(student) << " "
-             << get<3>(student) << " "
-             << get<4>(student) << " "
+        return eng_a > eng_b;  
+    });
+    
+    for (const auto& student : students) {
+        cout << get<0>(student) << " "  
+             << get<1>(student) << " " 
+             << get<2>(student) << " "  
+             << get<3>(student) << " " 
+             << get<4>(student) << " "  
              << get<5>(student) << endl;
     }
     return 0;
