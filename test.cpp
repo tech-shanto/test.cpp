d5e426cfe7c952a02095d282f0996ad24044911c
Jahid Hasan <jahid.hasan.shanto@|gmail.com>
2024-12-19 20:08:06 +0600

    linked list

--- test.cpp
+++ test.cpp
@@ -1,41 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+Node *createLinkedList()
+{
+    int value;
+    cin >> value;
+    if (value == -1)
+        return nullptr;
+    Node *head = new Node(value);
+    Node *current = head;
+
+    while (cin >> value && value != -1)
+    {
+        Node *newNode = new Node(value);
+        current->next = newNode;
+        current = newNode;
+    }
+    return head;
+}
+int findMaxMinDifference(Node *head)
+{
+    if (!head)
+        return 0;
+
+    int maxVal = INT_MIN;
+    int minVal = INT_MAX;
+    Node *current = head;
+    while (current)
+    {
+        maxVal = max(maxVal, current->data);
+        minVal = min(minVal, current->data);
+        current = current->next;
+    }
+    return maxVal - minVal;
+}
 int main()
 {
-   int N;
-    cin >> N;
-    vector<tuple<string, int, char, int, int, int>> students(N);
-    
-
-    for (int i = 0; i < N; ++i) {
-        string nm;
-        int cls, id, math_marks, eng_marks;
-        char s;
-        cin >> nm >> cls >> s >> id >> math_marks >> eng_marks;
-        students[i] = make_tuple(nm, cls, s, id, math_marks, eng_marks);
-    }
-    sort(students.begin(), students.end(), [](const auto& a, const auto& b) {
-        int eng_a = get<5>(a);
-        int eng_b = get<5>(b);
-        if (eng_a == eng_b) {
-            int math_a = get<4>(a);
-            int math_b = get<4>(b);
-            if (math_a == math_b) {
-                return get<3>(a) < get<3>(b);
-            }
-            return math_a > math_b; 
-        }
-        return eng_a > eng_b;  
-    });
-    
-    for (const auto& student : students) {
-        cout << get<0>(student) << " "  
-             << get<1>(student) << " " 
-             << get<2>(student) << " "  
-             << get<3>(student) << " " 
-             << get<4>(student) << " "  
-             << get<5>(student) << endl;
-    }
+    Node *head = createLinkedList();
+    cout << findMaxMinDifference(head) << endl;
     return 0;
 }
